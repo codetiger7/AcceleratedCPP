73ffb6f30d0f95d11ce24cd70deaad966f207d36
codetiger <briancatch@bigscreen.local>
2015-07-20 17:01:20 +0200

    8-1 first attempt

---
+++ 8.Writing Generic Functions/8-1_template_analysis/analysis.h
@@ -0,0 +1,42 @@
+#ifndef ANALYSIS_H
+#define ANALYSIS_H
+
+#include <vector>
+#include <string>
+#include <student_info.h>
+#include <iostream>
+#include <grade.h>
+#include <median.h>
+#include <numeric>
+
+
+void write_analysis(std::ostream& out,
+                    const std::string& name,
+                    double analysis(const std::vector<Student_info>&),
+                    const std::vector<Student_info>& did,
+                    const std::vector<Student_info>& didnt);
+
+
+
+double grade_aux(const Student_info& s);
+double optimistic_median(const Student_info& s);
+
+
+
+double median_analysis(const std::vector<Student_info>& students );
+double average_analysis(const std::vector<Student_info>& students);
+double optimistic_median_analysis(const std::vector<Student_info>& students);
+
+
+
+double average_grade(const Student_info& s);
+double average(std::vector<double>& v);
+
+
+
+
+bool did_all_hw(const Student_info& s);
+
+
+
+#endif // ANALYSIS_H
