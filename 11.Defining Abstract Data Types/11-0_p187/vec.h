67a1415105aa613325c3422de249a3f42855376b
codetiger <briancatch@bigscreen.local>
2015-07-24 16:24:25 +0200

    11.5 Flexible memory management starting

--- 11.Defining Abstract Data Types/11-0_p187/vec.h
+++ 11.Defining Abstract Data Types/11-0_p187/vec.h
@@ -6,8 +6,9 @@ template <class T>
 class Vec
 {
 public:
-    Vec& operator=(const Vec&);     // the assingment operator
-    Vec(const Vec& v) { create(v.begin(), v.end()); }          // copy constructor
+    ~Vec() { uncreate(); }
+    Vec& operator=(const Vec&);                                 // the assingment operator
+    Vec(const Vec& v) { create(v.begin(), v.end()); }           // copy constructor
 
     // typedef definitions §
     typedef T* iterator;
@@ -21,7 +22,7 @@ public:
     Vec() { create(); }
     explicit Vec(std::size_t n, const T& val = T())  { create(n, val); }
 
-    size_type size() const { return limit-data; }
+    size_type size() const { return avail-data; }
 
     T& operator[](size_type i)  { return data[i]; }
     const T& operator[](size_type i) const {return data[i]; }
@@ -30,12 +31,19 @@ public:
     iterator begin() { return data; }
     const_iterator begin() const { return data; }
 
-    iterator end() { return limit; }
-    const_iterator end() const { return limit; }
+    iterator end() { return avail; }
+    const_iterator end() const { return avail; }
+    void push_back(const T& val)
+    {
+        if (avail == limit)         // get space if needed
+            grow();
+        unchecked_append(val);      // append the new element
+    }
 
 private:
     iterator data;        // first element in the Vec
-    iterator limit;       // one past the last element in the Vec
+    iterator avail;       // pointer to (one past) the last constructed element
+    iterator limit;       // now points to (one past) last availiable element in the Vec
 };
 
 #endif // VEC_H
