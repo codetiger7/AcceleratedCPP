2ca858b6b9e9141b87613be23a39721a038ec3f5
codetiger7 <brianopedal@MacBook-Pro.local>
2015-07-24 20:24:08 +0200

    11 code

--- 11.Defining Abstract Data Types/11-0_p187/vec.cpp
+++ 11.Defining Abstract Data Types/11-0_p187/vec.cpp
@@ -1,10 +1,80 @@
-#include "vec.h"
+#include <vec.h>
+
+
+#include <algorithm>
+
+using std::max;
+
 
 Vec::Vec()
 {
 
 }
 
+template <class T>
+void Vec<T>::create()
+{
+    data = avail = limit = 0;
+}
+
+template <class T>
+void Vec<T>::create(size_type n, const T& val)
+{
+    data = alloc.allocate(n);
+    limit = avail = data + n;
+    unitialized_fill(data, limit, val);
+}
+
+template <class T>
+void Vec<T>::create(const_iterator i, const_iterator j)
+{
+    data = alloc.allocate(j-i);
+    limit = avail = uninitialized_copy(i, j, data);
+}
+
+template <class T>
+void Vec<T>::uncreate()
+{
+    if (data)
+    {
+        // destroy in reverse order, the elements that were constructed
+        iterator it = avail;
+        while (it != data)
+            alloc.destroy(--it);
+
+        // return all the space allocated
+        alloc.deallocate(data, limit - data);
+    }
+
+    // reset pointers to indicate that the Vec is empty again
+    data = limit = avail = 0;
+}
+
+template <class T>
+void Vec<T>::grow()
+{
+    // when growing, allocate twice as much space as currently in use
+    size_type new_size = max(2 * (limit-data), ptrdiff_t(1));
+
+    // allocate new space and copy existing elements to the new space
+    iterator new_data = alloc.allocate(new_size);
+    iterator new_avail = uninitialized_copy(data, avail, new_data);
+
+    // return the old space
+    uncreate();
+
+    // reset pointers
+    data = new_data;
+    avail = new_avail;
+    limit = data + new_size;
+}
+
+// assumes avail points at allocated, but uninitialized space
+template <class T>
+void Vec<T>::unchecked_append(const T& val)
+{
+    alloc.construct(avail++, val);
+}
 
 
 template <class T>
